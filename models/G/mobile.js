269b5947b04c2a842cd7857fd4f7a11221304f16
shahriar <shahriarenayaty1376@gmail.com>
2019-10-14 21:14:04 +0330

    fix unique title numbers

--- models/G/mobile.js
+++ models/G/mobile.js
@@ -4,15 +4,13 @@ const Schema = mongoose.Schema;
 const MobileSchema = new Schema({
     title: {
         type: String,
-        default: "personal",
-        unique: true
+        default: "mobiles"
     },
     number: {
         type: Number,
         required: true,
         unique: true
-        //description:{"شماره مویل را به صورت 09..و شماره خانه و مطب را با پیش شماره شهر وارد نمایید."}
-        //description:{"hello"}
+
     },
     status: {
         type: Boolean,
